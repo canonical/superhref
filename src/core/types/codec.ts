693e45a0f99d6a6eb0d824fe3637fda8b454223f
Mihaj <michael.kapustey@canonical.com>
2026-07-02 16:24:11 +0200

    License header

--- src/core/types/codec.ts
+++ src/core/types/codec.ts
@@ -1,20 +1,7 @@
-// This file is part of superhref, a typed, composable URL search-param state library.
-//
-// Copyright 2026 Canonical Ltd.
-//
-// SPDX-License-Identifier: LGPL-3.0-only
-//
-// This program is free software: you can redistribute it and/or modify it under
-// the terms of the GNU Lesser General Public License version 3, as published by
-// the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranties of MERCHANTABILITY, SATISFACTORY
-// QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-// License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License along
-// with this program.  If not, see http://www.gnu.org/licenses/.
+/*
+ * Copyright 2026 Canonical Ltd.  This software is licensed under the
+ * GNU Lesser General Public License version 3 (see the file LICENSE).
+ */
 
 /**
  * An encode/decode pair for a single search-param key.
